f86331d97ff7d2b2843fa8e4498c91b2ebcec14a
Jamie Gaehring <gaehj457@gmail.com>
2023-02-12 19:29:22 -0500

    Use tractor emoji as favicon and logo.

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -6,8 +6,15 @@ export default defineConfig({
   cacheDir: './.cache',
   outDir: './dist',
   title: 'Runrig',
+  head: [
+    [
+      'link',
+      { rel: 'icon', href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚜</text></svg>' }
+    ]
+  ],
   themeConfig: {
     siteTitle: 'Runrig',
+    logo: '/tractor.svg',
     nav: [
       { text: 'Overview', link: '/overview' },
       { text: 'Ecology', link: '/ecology' },
