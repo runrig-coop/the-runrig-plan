e1b502fddfb9e2db6f80eb4bce43f9b84e881192
Jamie Gaehring <gaehj457@gmail.com>
2023-04-16 15:41:06 -0400

    Update site config with page meta data.

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,20 +1,51 @@
 import footnote_plugin from 'markdown-it-footnote';
 import { defineConfig } from 'vitepress';
 
+const title = 'Runrig';
+const defaultObjectType = 'website';
+const canonical = 'https://www.runrig.org';
+const logo = '/emoji_u1f69c.svg';
+const cardImage = `${canonical}/open_field_system.png`;
+const description = `
+  Runrig is a communal farming platform, a libre software project and
+  a method of socio-ecological design. It aims for collective sovereignty over
+  not just our food, but also the labor that raises it, the land and living
+  things that support it, and the data connecting connecting it all.
+`.replaceAll(/\s+/g, ' ').trim();
+
 export default defineConfig({
   srcDir: './',
   cacheDir: './.cache',
   outDir: './dist',
-  title: 'Runrig',
+  title,
+  description,
   head: [
-    [
-      'link',
-      { rel: 'icon', href: '/emoji_u1f69c.svg' }
-    ]
+    ['link', { rel: 'icon', href: logo }],
+    ['link', { rel: 'canonical', href: canonical }],
+    /**
+     * OPEN GRAPH META TAGS
+     * @see https://ogp.me/
+     */
+    ['meta', { property: 'og:title', content: title }],
+    ['meta', { property: 'og:type', content: defaultObjectType }],
+    ['meta', { property: 'og:url', content: canonical }],
+    ['meta', { property: 'og:image', content: cardImage }],
+    ['meta', { property: 'og:description', content: description }],
+
+    /**
+     * TWITTER CARD META TAGS
+     * @see https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup
+     */
+    ['meta', { property: 'twitter:title', content: title }],
+    ['meta', { property: 'twitter:card', content: defaultObjectType }],
+    ['meta', { property: 'twitter:image', content: cardImage }],
+    ['meta', { property: 'twitter:description', content: description }],
+    // If we create a Twitter account, we can use this meta tag as well.
+    // ['meta', { property: 'twitter:creator', content: '@runrig_coop' }],
   ],
   themeConfig: {
-    siteTitle: 'Runrig',
-    logo: '/emoji_u1f69c.svg',
+    siteTitle: title,
+    logo,
     nav: [
       { text: 'Overview', link: '/overview' },
       { text: 'Ecology', link: '/ecology' },
