382c12d5b9224ce9cdba272fc515728cbd3a4e1c
Jamie Gaehring <jamie@runrig.org>
2025-05-13 18:44:27 -0400

    Use separate component for post index.

---
+++ .vitepress/theme/RRPostIndex.vue
@@ -0,0 +1,95 @@
+<script setup>
+const props = defineProps({ data: Array });
+
+function fmtDate(d) {
+  try {
+    const utc = new Date(d);
+    const offset = utc.getTimezoneOffset() * 60 * 1000;
+    const nineHours = 9 * 60 * 1000;
+    const localBizStartTime = new Date(utc.valueOf() + offset + nineHours);
+    const opts = { dateStyle: 'medium' };
+    return localBizStartTime.toLocaleDateString(undefined, opts);
+  } catch (e) {
+    console.warn(`Invalid date, ${d}, for post, "${fm.title}". Reason: `, e);
+    return false;
+  }
+}
+const posts = props.data.map((post) => {
+  const { frontmatter = {}, url } = post;
+  const { author, description, subtitle, title } = frontmatter;
+  const date = fmtDate(frontmatter.date);
+  return { author, date, description, subtitle, title, url };
+});
+</script>
+
+<template>
+  <main>
+    <a v-for="(post, i) of posts" :key="`post-${i}`" :href="post.url">
+      <header>
+        <h1>{{ post.title }}</h1>
+        <h2 v-if="post.subtitle">{{ post.subtitle }}</h2>
+      </header>
+      <ul v-if="post.author || post.date">
+        <li v-if="post.author">{{ post.author }}</li>
+        <li v-if="post.date">{{ post.date }}</li>
+      </ul>
+      <p v-if="post.description">
+        {{ post.description }}
+      </p>
+    </a>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding-top: 6rem;
+}
+a {
+  display: inline-block;
+  min-width: 100%;
+  color: var(--vp-c-text-2);
+  text-decoration: none;
+  text-decoration-color: transparent;
+  text-decoration-style: unset;
+  text-underline-offset: 0;
+  border-bottom: 1px solid var(--vp-c-divider);
+  margin-bottom: 3rem;
+}
+header {
+  margin-bottom: .75rem
+}
+h1 {
+  font-size: 3rem;
+  line-height: 3rem;
+  color: var(--vp-c-text-1);
+}
+h2 {
+  border: none;
+  margin: .5rem 0 0 0;
+  padding: 0;
+  color: var(--vp-c-text-2);
+  letter-spacing: -0.02em;
+  line-height: 28px;
+  font-size: 24px;
+  max-width: 80%;
+}
+ul, li {
+  all: unset;
+}
+ul {
+  margin: 0 0 .75rem 0;
+  list-style: none;
+  padding-left: 0;
+}
+li:nth-child(1) {
+  color: var(--vp-c-text-1);
+  margin-right: .75rem;
+}
+li:nth-child(2) {
+  color: var(--vp-c-text-1);
+  color: var(--vp-c-text-2);
+}
+p {
+  margin: 1.5rem 0;
+}
+</style>
--- posts.md
+++ posts.md
@@ -1,28 +1,6 @@
 <script setup>
-import { data as posts } from './.vitepress/theme/posts.data.js';
-import RRPostHeader from './.vitepress/theme/RRPostHeader.vue';
+import { data } from './.vitepress/theme/posts.data.js';
+import RRPostIndex from './.vitepress/theme/RRPostIndex.vue';
 </script>
 
-<div v-for="post of posts" class="post-list">
-  <a :href="post.url">
-    <RRPostHeader :fm="post.frontmatter" >
-      <span v-if="post.frontmatter?.description" class="description">
-        {{ post.frontmatter.description }}
-      </span>
-    </RRPostHeader>
-  </a>
-</div>
-
-<style scoped>
-.post-list a {
-  color: var(--vp-c-text-2);
-  text-decoration: none;
-  text-decoration-color: transparent;
-  text-decoration-style: unset;
-  text-underline-offset: 0;
-}
-.description {
-  display: inline-block;
-  margin-bottom: .75rem;
-}
-</style>
+<RRPostIndex :data="data" />
