99486d7b52d1252d700628fe9dc6187d2636ec62
Jamie Gaehring <jamie@runrig.org>
2025-05-13 20:48:57 -0400

    Filter and sort posts by date.

--- .vitepress/theme/posts.data.js
+++ .vitepress/theme/posts.data.js
@@ -1,3 +1,11 @@
 import { createContentLoader } from 'vitepress'
 
-export default createContentLoader('posts/*.md');
+const hasValidDate = post => !!new Date(post.frontmatter.date).valueOf();
+
+const sortPostsDescByDate = (a, b) =>
+  +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date);
+
+const transform = raw =>
+  raw.filter(hasValidDate).toSorted(sortPostsDescByDate);
+
+export default createContentLoader('posts/*.md', { transform });
