6283e2eac4f75e6dc162acdac5fc6186ae20f09f
Jamie Gaehring <jamie@runrig.org>
2025-05-20 08:58:36 -0400

    Reference jpmonette/feed example for RSS.

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,5 +1,90 @@
 import footnote_plugin from 'markdown-it-footnote';
 import { defineConfig } from 'vitepress';
+/**
+ * @todo npm i feed
+ * @see https://github.com/jpmonette/feed
+ */
+// import { Feed } from "feed";
+
+/**
+ * @todo Maybe instantiate the new Feed() object here?
+ * @see https://github.com/jpmonette/feed#example
+ */
+// const feed = new Feed({
+//   title: "Feed Title",
+//   description: "This is my personal feed!",
+//   id: "http://example.com/",
+//   link: "http://example.com/",
+//   language: "en", // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+//   image: "http://example.com/image.png",
+//   favicon: "http://example.com/favicon.ico",
+//   copyright: "All rights reserved 2013, John Doe",
+//   updated: new Date(2013, 6, 14), // optional, default = today
+//   generator: "awesome", // optional, default = 'Feed for Node.js'
+//   feedLinks: {
+//     json: "https://example.com/json",
+//     atom: "https://example.com/atom"
+//   },
+//   author: {
+//     name: "John Doe",
+//     email: "johndoe@example.com",
+//     link: "https://example.com/johndoe"
+//   }
+// });
+
+// posts.forEach(post => {
+//   feed.addItem({
+//     title: post.title,
+//     id: post.url,
+//     link: post.url,
+//     description: post.description,
+//     content: post.content,
+//     author: [
+//       {
+//         name: "Jane Doe",
+//         email: "janedoe@example.com",
+//         link: "https://example.com/janedoe"
+//       },
+//       {
+//         name: "Joe Smith",
+//         email: "joesmith@example.com",
+//         link: "https://example.com/joesmith"
+//       }
+//     ],
+//     contributor: [
+//       {
+//         name: "Shawn Kemp",
+//         email: "shawnkemp@example.com",
+//         link: "https://example.com/shawnkemp"
+//       },
+//       {
+//         name: "Reggie Miller",
+//         email: "reggiemiller@example.com",
+//         link: "https://example.com/reggiemiller"
+//       }
+//     ],
+//     date: post.date,
+//     image: post.image
+//   });
+// });
+
+// feed.addCategory("Technologie");
+
+// feed.addContributor({
+//   name: "Johan Cruyff",
+//   email: "johancruyff@example.com",
+//   link: "https://example.com/johancruyff"
+// });
+
+// console.log(feed.rss2());
+// // Output: RSS 2.0
+
+// console.log(feed.atom1());
+// // Output: Atom 1.0
+
+// console.log(feed.json1());
+// // Output: JSON Feed 1.0
+
 
 const title = 'Runrig';
 const defaultObjectType = 'website';
@@ -75,5 +160,12 @@ export default defineConfig({
     config(md) {
       md.use(footnote_plugin);
     }
+  },
+  async transformHead(ctx) {
+    const { frontmatter, relativePath, filePath } = ctx.pageData;
+    /**
+     * @todo Write metadata to feed.rss for each page
+     * @see https://vitepress.dev/reference/site-config#transformhead
+     */
   },
 });
