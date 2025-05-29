bdcfe558e61eb3b0f11dfeb5fbd493038d8ee220
Jamie Gaehring <jamie@runrig.org>
2025-05-28 22:56:56 -0400

    Add an empty Projects menu heading to main nav.

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -104,6 +104,11 @@ export default defineConfig({
     siteTitle: title,
     logo,
     nav: [
+      {
+        text: 'Projects',
+        items: [
+        ],
+      },
       { text: 'The Plan', link: '/posts/the-runrig-plan-for-socio-ecological-design' },
       { text: 'Journal', link: '/journal' },
       {
