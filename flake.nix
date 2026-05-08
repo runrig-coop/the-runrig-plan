369600d23a1ddc8d9c4c82b802b1d3a3377ea399
Jamie Gaehring <jamie@runrig.org>
2026-05-08 17:18:53 -0400

    Add nix flake for Node dev env.

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  # Nix flake based on the Node dev template from the-nix-way/dev-templates:
+  # https://github.com/the-nix-way/dev-templates/tree/d94a04795dfce731aada15bbf1714389cc07ba23/node
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: rec {
+        nodejs = prev.nodejs;
+        yarn = (prev.yarn.override { inherit nodejs; });
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              nodejs
+              pnpm
+              yarn
+              self.formatter.${system}
+            ];
+          };
+        }
+      );
+
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
+    };
+}
